e80e4fe7ec6d98df77f4378d69d97a09776e3331
evandrvalente <evandr.valente@gmail.com>
2022-08-08 22:35:13 -0300

    feat: add src/controllers/books.controllers.ts

--- src/routes/books.routes.ts
+++ src/routes/books.routes.ts
@@ -1,5 +1,9 @@
 import { Router } from 'express';
+import BooksController from '../controllers/books.controller';
 
+const booksController = new BooksController()
 const BooksRouter = Router();
+
+BooksRouter.get('/books', booksController.getAll);
 
 export default BooksRouter;
